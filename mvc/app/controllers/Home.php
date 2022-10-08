aee13350f69add2e896c8c47f74b30566c3a5fdd
dzayeate <mr.x685@gmail.com>
2022-10-08 16:29:12 +0700

    Vidio #5 View

--- mvc/app/controllers/Home.php
+++ mvc/app/controllers/Home.php
@@ -1,8 +1,13 @@
 <?php
 
-class Home {
+class Home extends Controller
+{
   public function index()
   {
-    echo 'home/index';
+    $data['judul'] = 'Home';
+    $data['nama'] = 'Agam ';
+    $this->view('templates/header', $data);
+    $this->view('home/index', $data);
+    $this->view('templates/footer');
   }
 }
