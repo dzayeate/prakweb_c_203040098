d8ba6dc5749e6737bd1e875e54849380daad1d4f
ARKA <mr.x685@gmail.com>
2023-10-14 12:10:27 +0700

    Final Update

--- oop/Inheritance-problem.php
+++ oop/Inheritance-problem.php
@@ -1,53 +1,48 @@
-<?php
+<?php 
 
-class Produk
-{
-  public $judul,
-    $penulis,
-    $penerbit,
-    $harga,
-    $jmlHalaman,
-    $waktuMain,
-    $tipe;
+class Produk {
+    public $judul, 
+           $penulis,
+           $penerbit,
+           $harga,
+           $jmlHalaman,
+           $waktuMain,
+           $tipe;
 
-  public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0, $waktuMain = 0, $tipe)
-  {
-    $this->judul = $judul;
-    $this->penulis = $penulis;
-    $this->penerbit = $penerbit;
-    $this->harga = $harga;
-    $this->jmlHalaman = $jmlHalaman;
-    $this->waktuMain = $waktuMain;
-    $this->tipe = $tipe;
-  }
-
-  public function getLabel()
-  {
-    return "$this->penulis, $this->penerbit";
-  }
-
-  public function getInfoLengkap()
-  {
-    // Komik : Naruto | Mashashi Kishimoto, Shonen Jump (Rp. 30000) - 100 Halaman.
-    $str = "{$this->tipe} : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
-    if ($this->tipe == "Komik") {
-      $str .=  " - {$this->jmlHalaman} Halaman.";
-    } else if ($this->tipe == "Game") {
-      $str .= " ~ {$this->waktuMain} Jam.";
+    public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0, $waktuMain = 0, $tipe ) {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+        $this->jmlHalaman = $jmlHalaman;
+        $this->waktuMain = $waktuMain;
+        $this->tipe = $tipe;
     }
 
-    return $str;
-  }
+    public function getLabel() {
+        return "$this->penulis, $this->penerbit";
+    }
+
+    public function getInfoLengkap() {
+        // Komik : Naruto | Mashashi Kishimoto, Shonen Jump (Rp. 30000) - 100 Halaman.
+        $str = "{$this->tipe} : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+        if( $this->tipe == "Komik" ) {
+            $str .=  " - {$this->jmlHalaman} Halaman.";
+        } else if( $this->tipe == "Game" ) {
+            $str .= " ~ {$this->waktuMain} Jam.";
+        }
+
+        return $str;
+    }
+
 }
 
 
-class CetakInfoProduk
-{
-  public function cetak(Produk $produk)
-  {
-    $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
-    return $str;
-  }
+class CetakInfoProduk {
+    public function cetak( Produk $produk ) {
+        $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+        return $str;
+    }
 }
 
 
@@ -57,3 +52,6 @@ $produk2 = new Produk("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 0,
 echo $produk1->getInfoLengkap();
 echo "<br>";
 echo $produk2->getInfoLengkap();
+
+
+
